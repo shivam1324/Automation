97855b0b56f8a00939e1253c2d1a30e257a4fdb0
Shivam <shivamgarg1324@gmail.com>
2021-10-25 11:50:18 +0530

    learned excel commands

---
+++ app.py
@@ -0,0 +1,12 @@
+import openpyxl as xl
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']   # cell = sheet.cell(1,1)
+#for value -> cell.value
+for row in range(2,sheet.max_row +1):
+    cell = sheet.cell(row,3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transacttions2.xlsx')
